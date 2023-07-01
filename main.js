008609abe5631d0b359f80b8ab2ff55ef43462a0
Swara711 <73833213+Swara711@users.noreply.github.com>
2023-07-01 20:48:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status1= "";
+objects= [];
+
+function setup(){
+    canvas= createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status1= true;
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
